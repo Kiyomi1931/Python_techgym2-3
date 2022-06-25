49e2a0d51ffb339bec5c56801f8e28fd6bd79c3d
Kiyomi1931 <71973734+Kiyomi1931@users.noreply.github.com>
2022-06-25 11:30:11 +0900

    Python2-3-3 個人解答

--- techgym2-3-3.py
+++ techgym2-3-3.py
@@ -1,5 +1,6 @@
 import random
 
+data = [['見','貝'],['土','士'],['眠','眼']]
 level = 1
 
 def start_message():
@@ -8,8 +9,13 @@ def start_message():
 def section_message():
   print('レベル:' + str(level))
 
+def view_question():
+  question_number = random.randint(0,2)
+  print(data[question_number])
+
 def play():
   section_message()
+  view_question()
   choice = input('(例:A1)')
   print('デバッグ:choice = ' + choice)
 
